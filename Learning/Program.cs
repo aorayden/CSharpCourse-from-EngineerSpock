29d01ae9373c1f6d84b1150bd90d972f5f61a373
aorayden <aorayden@gmail.com>
2024-09-27 22:03:56 +0500

    Добавьте файлы проекта.

---
+++ Learning/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                uint x = uint.MaxValue;
+
+                Console.WriteLine(x); // Вывод: 4294967295.
+
+                x = x + 1;
+
+                Console.WriteLine(x); // Когда мы переходим максимально доступное значение происходит переполнение и мы возвращаемся к минимальному возможному: 0.
+
+                x = x - 1;
+
+                Console.WriteLine(x); // Вывод: 4294967295.
+            }
+        }
+    }
+}
